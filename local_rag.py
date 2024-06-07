1c42e5815a35f7b193210af32ee28c14fcdae529
6aligula <vinicio.naranjo15@gmail.com>
2024-06-07 11:36:21 +0200

    response ok of model ollama 3, but prompt is not good

--- local_rag.py
+++ local_rag.py
@@ -1,6 +1,6 @@
 from langchain_community.llms import Ollama
 
-#llm = Ollama(model="llama3")
+llm = Ollama(model="llama3")
 
 # response = llm.invoke("Hola, quien eres?")
 
@@ -9,7 +9,7 @@ from langchain_community.llms import Ollama
 # load pdf in parts
 from langchain_community.document_loaders import PyMuPDFLoader
 
-loader = PyMuPDFLoader("CV_Andres_2024.pdf")
+loader = PyMuPDFLoader("src/CV_Andres_2024.pdf")
 data_pdf = loader.load()
 #print(data_pdf[0])
 
@@ -17,6 +17,51 @@ data_pdf = loader.load()
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=50)
 docs = text_splitter.split_documents(data_pdf)
-print(f"primera parte:\n {docs[0]} \n")
-print("segunnda parte \n")
-print(docs[1])
+# print(f"primera parte:\n {docs[0]} \n")
+# print("segunnda parte \n")
+# print(docs[1])
+
+# create embedings form the last chung
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+embed_model = FastEmbedEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# chroma database for ewmbedings
+from langchain_community.vectorstores import Chroma
+vs = Chroma.from_documents(
+    documents=docs,
+    embedding=embed_model,
+    persist_directory="chroma_db_dir",  # Local mode with in-memory storage only
+    collection_name="cv_data"
+)
+vectorstore = Chroma(embedding_function=embed_model,
+                     persist_directory="chroma_db_dir",
+                     collection_name="cv_data")
+retriever=vectorstore.as_retriever(search_kwargs={'k': 3}) # numero de chunks que devuelve es 3
+
+#creando el prompt
+
+from langchain.prompts import PromptTemplate
+
+custom_prompt_template = """Usa la siguiente información para responder a la pregunta del usuario.
+Si no sabes la respuesta, di que no lo se, no inventes una respuesta.
+
+Contexto: {context}
+Pregunta: {question}
+
+Solo devuelve la respuesta útil a continuación y nada más
+Respuesta útil:
+"""
+prompt = PromptTemplate(template=custom_prompt_template,
+                        input_variables=['context', 'question'])
+
+# hacer un retreival para construir question y answer contra el modelo ollama 3
+from langchain.chains import RetrievalQA
+
+qa = RetrievalQA.from_chain_type(llm=llm,
+                                 chain_type="stuff",
+                                 retriever=retriever,
+                                 return_source_documents=True,
+                                 chain_type_kwargs={"prompt": prompt})
+
+response = qa.invoke({"query": "Cual son los trabajos desempeñados por el dueño del curriculum"})
+print(response["result"])
